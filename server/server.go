ecafa99ab0a658cbec530fd46fd37c491f172826
Naoki Kishi <naokikishi.kyoto@gmail.com>
2019-06-25 16:51:27 +0900

    Jobを返すハンドラーを実装

---
+++ server/server.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"bufio"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+type Server struct {
+	dir      string
+	timeMemo map[string]string
+	port     int
+}
+
+func NewServer(dir string, port int) *Server {
+	s := &Server{dir, map[string]string{}, port}
+	s.readJob()
+	return s
+}
+
+func (s *Server) Start() error {
+	http.HandleFunc("/", s.GetJobHandler())
+	log.Println("Starting server...")
+	return http.ListenAndServe(":"+strconv.Itoa(s.port), nil)
+}
+
+func (s *Server) readJob() {
+	files, err := ioutil.ReadDir(s.dir)
+	if err != nil {
+		log.Fatalf("failed to read job files : %#v", err) // jobを読み込めなかったらサーバを落としてよい。
+	}
+
+	for _, f := range files {
+		fp, err := os.Open(filepath.Join(s.dir, f.Name()))
+		if err != nil {
+			log.Fatalf("failed to read job files : %#v", err) // jobを読み込めなかったらサーバを落としてよい。
+		}
+		defer fp.Close()
+
+		scanner := bufio.NewScanner(fp)
+
+		for scanner.Scan() {
+			if scanner.Text() == "[Created]" {
+				scanner.Scan()
+				s.timeMemo[scanner.Text()] = f.Name()
+			}
+		}
+
+		if err = scanner.Err(); err != nil {
+			log.Fatalf("failed to read job files : %#v", err) // jobを読み込めなかったらサーバを落としてよい。
+		}
+	}
+
+}
