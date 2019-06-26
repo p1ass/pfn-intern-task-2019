f148274c821700776c6a4849cb0ca6e900964c7f
Naoki Kishi <naokikishi.kyoto@gmail.com>
2019-06-26 13:56:08 +0900

    2-2のグラフを作成

--- worker/output/generate_graph.py
+++ worker/output/generate_graph.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 
 def main():
-    df = pd.read_csv("2-1_executing_point.csv", encoding="UTF-8", delimiter=',' ,header=None)
+    df = pd.read_csv("2-2_executing_point.csv", encoding="UTF-8", delimiter=',' ,header=None)
     register_matplotlib_converters()
     df[0] = pd.to_datetime(df[0])
 
@@ -12,6 +12,6 @@ def main():
     plt.plot(df[0],df[1])
     plt.xlabel("Timestamp")
     plt.ylabel("Executing Point")
-    plt.savefig("2-1.png")
+    plt.savefig("2-2.png")
 if __name__ == "__main__":
     main()
