4be8189ca2c0cfc5083da9502be899c3a62c1f39
Naoki Kishi <naokikishi.kyoto@gmail.com>
2019-06-26 13:59:28 +0900

    JobサーバのポートとJobデータのディレクトリをコマンドライン引数で指定できるようにした

--- job_server/main.go
+++ job_server/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/naoki-kishi/pfn-intern-task-2019/job_server/server"
 )
 
 func main() {
-	s := server.NewServer("./data", 8080)
+	port := flag.Int("p", 8080, "Server port number")
+	dir := flag.String("d", "./data", "Job data directory")
+	flag.Parse()
+
+	s := server.NewServer(*dir, *port)
 
 	log.Fatal(s.Start())
 }
